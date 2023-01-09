6bd916de74942f868aabdf8a62a385f027d92b54
Jürgen Sieben <j.sieben@condes.de>
2023-01-09 15:25:20 +0100

    Update and rename MessageGroup_ORACLE.sql to MessageGroup_ORA.sql

--- ADC/core/messages/GERMAN/MessageGroup_ORA.sql
+++ ADC/core/messages/GERMAN/MessageGroup_ORA.sql
@@ -1,12 +1,12 @@
 begin
 
   pit_admin.merge_message_group(
-    p_pmg_name => 'ORACLE',
+    p_pmg_name => 'ORA',
     p_pmg_description => q'^Meldungen für Oracle-Fehler^');
 
   pit_admin.merge_message(
-    p_pms_name => 'CHILD_RECORD_FOUND',
-    p_pms_pmg_name => 'ORACLE',
+    p_pms_name => 'ORA_CHILD_RECORD_FOUND',
+    p_pms_pmg_name => 'ORA',
     p_pms_text => q'^Der Datensatz konnte nicht gelöscht werden, es existieren abhängige Datensätze.^',
     p_pms_description => q'^^',
     p_pms_pse_id => 30,
@@ -14,8 +14,8 @@ begin
     p_error_number => -2292);
 
   pit_admin.merge_message(
-    p_pms_name => 'CONVERSION_IMPOSSIBLE',
-    p_pms_pmg_name => 'ORACLE',
+    p_pms_name => 'ORA_CONVERSION_IMPOSSIBLE',
+    p_pms_pmg_name => 'ORA',
     p_pms_text => q'^Eine Umwandlung konnte nicht ausgeführt werden^',
     p_pms_description => q'^^',
     p_pms_pse_id => 20,
@@ -23,8 +23,8 @@ begin
     p_error_number => -20000);
 
   pit_admin.merge_message(
-    p_pms_name => 'INVALID_DATE',
-    p_pms_pmg_name => 'ORACLE',
+    p_pms_name => 'ORA_INVALID_DATE',
+    p_pms_pmg_name => 'ORA',
     p_pms_text => q'^Ungültiges Datum: #1#^',
     p_pms_description => q'^^',
     p_pms_pse_id => 30,
@@ -32,7 +32,7 @@ begin
     p_error_number => -1858);
 
   pit_admin.merge_message(
-    p_pms_name => 'INVALID_DATE_FORMAT',
+    p_pms_name => 'ORA_INVALID_DATE_FORMAT',
     p_pms_pmg_name => 'ORACLE',
     p_pms_text => q'^Ungültiges Datumsformat: #1#^',
     p_pms_description => q'^^',
@@ -41,8 +41,8 @@ begin
     p_error_number => -1821);
 
   pit_admin.merge_message(
-    p_pms_name => 'INVALID_DATE_LENGTH',
-    p_pms_pmg_name => 'ORACLE',
+    p_pms_name => 'ORA_INVALID_DATE_LENGTH',
+    p_pms_pmg_name => 'ORA',
     p_pms_text => q'^Ungültige Länge des Datums: #1#^',
     p_pms_description => q'^^',
     p_pms_pse_id => 30,
@@ -50,8 +50,8 @@ begin
     p_error_number => -1840);
 
   pit_admin.merge_message(
-    p_pms_name => 'INVALID_DAY',
-    p_pms_pmg_name => 'ORACLE',
+    p_pms_name => 'ORA_INVALID_DAY',
+    p_pms_pmg_name => 'ORA',
     p_pms_text => q'^#1#^',
     p_pms_description => q'^^',
     p_pms_pse_id => 30,
@@ -59,8 +59,8 @@ begin
     p_error_number => -1847);
 
   pit_admin.merge_message(
-    p_pms_name => 'INVALID_DAY_FOR_MONTH',
-    p_pms_pmg_name => 'ORACLE',
+    p_pms_name => 'ORA_INVALID_DAY_FOR_MONTH',
+    p_pms_pmg_name => 'ORA',
     p_pms_text => q'^#1#^',
     p_pms_description => q'^^',
     p_pms_pse_id => 30,
@@ -68,8 +68,8 @@ begin
     p_error_number => -1839);
 
   pit_admin.merge_message(
-    p_pms_name => 'INVALID_MONTH',
-    p_pms_pmg_name => 'ORACLE',
+    p_pms_name => 'ORA_INVALID_MONTH',
+    p_pms_pmg_name => 'ORA',
     p_pms_text => q'^#1#^',
     p_pms_description => q'^^',
     p_pms_pse_id => 30,
@@ -77,8 +77,8 @@ begin
     p_error_number => -1843);
 
   pit_admin.merge_message(
-    p_pms_name => 'INVALID_NUMBER_FORMAT',
-    p_pms_pmg_name => 'ORACLE',
+    p_pms_name => 'ORA_INVALID_NUMBER_FORMAT',
+    p_pms_pmg_name => 'ORA',
     p_pms_text => q'^Ungültiges Zahlformat: #1#^',
     p_pms_description => q'^^',
     p_pms_pse_id => 30,
@@ -86,8 +86,8 @@ begin
     p_error_number => -1481);
 
   pit_admin.merge_message(
-    p_pms_name => 'INVALID_YEAR',
-    p_pms_pmg_name => 'ORACLE',
+    p_pms_name => 'ORA_INVALID_YEAR',
+    p_pms_pmg_name => 'ORA',
     p_pms_text => q'^#1#^',
     p_pms_description => q'^^',
     p_pms_pse_id => 30,
@@ -95,8 +95,8 @@ begin
     p_error_number => -1841);
 
   pit_admin.merge_message(
-    p_pms_name => 'SQL_ACCESS_DENIED',
-    p_pms_pmg_name => 'ORACLE',
+    p_pms_name => 'ORA_SQL_ACCESS_DENIED',
+    p_pms_pmg_name => 'ORA',
     p_pms_text => q'^#1#^',
     p_pms_description => q'^^',
     p_pms_pse_id => 30,
