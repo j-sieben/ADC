fe8a486f92e2b5123a276f1d604bd8ffcc1c3151
Jürgen Sieben <j.sieben@condes.de>
2023-01-09 15:20:26 +0100

    Update MessageGroup_ADCA.sql

--- ADC/apex/messages/GERMAN/MessageGroup_ADCA.sql
+++ ADC/apex/messages/GERMAN/MessageGroup_ADCA.sql
@@ -7,7 +7,7 @@ begin
     p_pmg_error_postfix => 'ERR');
 
   pit_admin.merge_message(
-    p_pms_name => 'ADCA_UI_UNSAVED_DATA',
+    p_pms_name => 'ADCA_UNSAVED_DATA',
     p_pms_pmg_name => 'ADCA',
     p_pms_text => q'^Es existieren ungesicherte Eingaben auf der Anwendungsseite. Bitte bestätigen Sie, dass diese verworfen werden sollen.^',
     p_pms_description => q'^Wird ausgelöst, falls ungesicherte Änderungen auf der Anwendungsseite verloren gehen könnten.^',
@@ -15,7 +15,7 @@ begin
     p_pms_pml_name => 'GERMAN');
 
   pit_admin.merge_message(
-    p_pms_name => 'ADCA_UI_CONFIRM_DELETE',
+    p_pms_name => 'ADCA_CONFIRM_DELETE',
     p_pms_pmg_name => 'ADCA',
     p_pms_text => q'^Bitte bestätigen Sie, dass die Daten gelöscht werden sollen.^',
     p_pms_description => q'^Wird ausgelöst, wenn eine Löschoperation ausgelöst wird, um umgewolltes Löschen zu vermeiden.^',
@@ -23,7 +23,7 @@ begin
     p_pms_pml_name => 'GERMAN');
 
   pit_admin.merge_message(
-    p_pms_name => 'ADCA_UI_ACTION_REQUESTED',
+    p_pms_name => 'ADCA_ACTION_REQUESTED',
     p_pms_pmg_name => 'ADCA',
     p_pms_text => q'^#1#-Aktion für #2# angefordert.^',
     p_pms_description => q'^Information über die Aktion die durch die Logik ermittelt wurde.^',
@@ -31,7 +31,7 @@ begin
     p_pms_pml_name => 'GERMAN');
 
   pit_admin.merge_message(
-    p_pms_name => 'ADCA_UI_UNKNOWN_ACTION',
+    p_pms_name => 'ADCA_UNKNOWN_ACTION',
     p_pms_pmg_name => 'ADCA',
     p_pms_text => q'^Unbekannte Aktion #1# angefordert.^',
     p_pms_description => q'^Programmierfehler.^',
@@ -39,7 +39,7 @@ begin
     p_pms_pml_name => 'GERMAN');
 
   pit_admin.merge_message(
-    p_pms_name => 'ADCA_UI_CHANGES_SAVED',
+    p_pms_name => 'ADCA_CHANGES_SAVED',
     p_pms_pmg_name => 'ADCA',
     p_pms_text => q'^Änderungen gespeichert.^',
     p_pms_description => q'^Erfolgsmeldung, falls eine Datenänderung erfolgreich gespeichert werden konnte.^',
@@ -47,7 +47,7 @@ begin
     p_pms_pml_name => 'GERMAN');
 
   pit_admin.merge_message(
-    p_pms_name => 'ADCA_UI_DATA_DELETED',
+    p_pms_name => 'ADCA_DATA_DELETED',
     p_pms_pmg_name => 'ADCA',
     p_pms_text => q'^Daten wurden gelöscht.^',
     p_pms_description => q'^Erfolgsmeldung, falls Daten erfolgreich gelöscht werden konnten.^',
@@ -55,7 +55,7 @@ begin
     p_pms_pml_name => 'GERMAN');
 
   pit_admin.merge_message(
-    p_pms_name => 'ADCA_UI_CHK_REGISTER_OBSERVER',
+    p_pms_name => 'ADCA_CHK_REGISTER_OBSERVER',
     p_pms_pmg_name => 'ADCA',
     p_pms_text => q'^Das Element #1# muss nicht beobachtet werden, da eine technische Bedingung das Element referenziert.^',
     p_pms_description => q'^Die Aktion kann entfernt werden, ohne das Verhalten der dynamischen Seite zu ändern.^',
@@ -71,7 +71,7 @@ begin
     p_pms_pml_name => 'GERMAN');
 
   pit_admin.merge_message(
-    p_pms_name => 'ADCA_UI_CHK_MISSING',
+    p_pms_name => 'ADCA_CHK_MISSING',
     p_pms_pmg_name => 'ADCA',
     p_pms_text => q'^Das Element #1# existiert auf der Seite nicht.^',
     p_pms_description => q'^Eventuell wurde das Element gelöscht oder umbenannt. Bitte kontrollieren Sie dies, da ansonsten JavaScript-Fehler auf der Seite auftauchen.^',
@@ -79,7 +79,7 @@ begin
     p_pms_pml_name => 'GERMAN');
 
   pit_admin.merge_message(
-    p_pms_name => 'ADCA_UI_CONFIRM_DELETE',
+    p_pms_name => 'ADCA_CONFIRM_DELETE',
     p_pms_pmg_name => 'ADCA',
     p_pms_text => q'^Bitte bestätigen Sie, dass der Datensatz gelöscht werden soll.^',
     p_pms_description => q'^Sicherheitsmeldung vor dem Löschen eines Datensatzes.^',
