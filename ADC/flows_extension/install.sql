490f55efa8e69c41d5347f62194bca4baeaa379d
Jürgen Sieben <j.sieben@condes.de>
2024-06-02 13:42:47 +0200

    Dependency on flows for apex removed

--- ADC/flows_extension/install.sql
+++
@@ -1,9 +0,0 @@
-@&tool_dir.set_folder flows_extension
-
-prompt &h2.Remove existing installation
-@&install_dir.uninstall.sql
-
-prompt &h2.Create database objects
-
-prompt &h2.Create Package dependent Views
-@&tool_dir.create_view adca_ui_designer_tree
