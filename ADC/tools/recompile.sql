a968c724f3887919f02a8571f73e45284d833432
Jürgen Sieben <j.sieben@condes.de>
2025-01-02 14:06:56 +0100

    New recompile script

--- ADC/tools/recompile.sql
+++ ADC/tools/recompile.sql
@@ -1,11 +1,22 @@
 
 prompt &h1.Recompiling invalid objects
 declare
+  cursor obj_cur is
+    select object_type, object_name,
+           case when instr(object_type, 'BODY') = 0 then 1 else 2 end recompile_order
+      from user_objects
+     where status = 'INVALID'
+       and object_name like 'ADC%'
+     order by recompile_order;
   l_invalid_objects binary_integer;
 begin
-  dbms_utility.compile_schema(
-    schema => user,
-    compile_all => false);
+  for o in obj_cur loop
+    if o.recompile_order = 1 then
+      execute immediate 'alter ' || o.object_type || ' compile';
+    else
+      execute immediate 'alter ' || o.object_type || ' compile body';
+    end if;
+  end loop;
     
   select count(*)
     into l_invalid_objects
