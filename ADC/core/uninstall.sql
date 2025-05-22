3b4f8302462df0cbf5615b59c88e4d697f68f57d
Jürgen Sieben <j.sieben@condes.de>
2025-05-22 08:43:51 +0200

    Install scripts corrected

--- ADC/core/uninstall.sql
+++ ADC/core/uninstall.sql
@@ -15,7 +15,7 @@ declare
            where (object_name in (
                  'ADC_BASIC', 'ADC', -- Typen
                  'ADC_ADMIN', 'ADC_INTERNAL', 'ADC_UTIL', 'ADC_VALIDATION', 'ADC_API', 'ADC_APEX_ACTION', 'ADC_UT', 
-                 'ADC_RECURSION_STACK', 'ADC_PAGE_STATE', 'ADC_PARAMETER', 'ADC_RESPONSE',-- Packages
+                 'ADC_ACTIONS', 'ADC_RECURSION_STACK', 'ADC_PAGE_STATE', 'ADC_PARAMETER', 'ADC_RESPONSE',-- Packages
                  'ADC_RULE_GROUP_STATUS', 'ADC_BL_PAGE_ITEMS', 'ADC_BL_PAGE_TARGETS', 'ADC_BL_RULES', 'ADC_BL_CAT_HELP', 'ADC_ACTION_PARAM_VISUAL_TYPES_V', 
                  'ADC_ACTION_ITEM_FOCUS_V', 'ADC_ACTION_PARAM_TYPES_V', 'ADC_ACTION_PARAMETERS_V', 'ADC_ACTION_TYPES_V', 'ADC_ACTION_TYPE_GROUPS_V', 
                  'ADC_BL_BIND_ITEMS', 'ADC_EVENT_TYPES_V', 'ADC_STANDARD_MESSAGES_V', 
@@ -51,6 +51,8 @@ begin
   pit_admin.delete_message_group('ADC', true);
   param_admin.delete_parameter_group('ADC', true);  
   utl_text_admin.delete_template('ADC');
+  
+  commit;
   
 end;
 /
